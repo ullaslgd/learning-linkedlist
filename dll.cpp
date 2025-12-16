6a36d084c80503f2716d2d05e0616c45544082d1
ullas <clubullas@gmail.com>
2025-12-16 10:58:01 +0530

    add deleteNode

--- dll.cpp
+++ dll.cpp
@@ -87,6 +87,22 @@ Node* deleteK(Node* head, int k){
     delete temp;return head;
 }
 
+void deleteNode(Node* temp){
+    Node * prev = temp->back;
+    Node* front = temp->next;
+
+    if(front == NULL){
+        prev->next = nullptr;
+        temp->back = nullptr;
+        free(temp); return;
+    }
+    prev->next = front;
+    front->back = prev;
+
+    temp->next = temp ->back = nullptr;
+    free(temp); return;
+}
+
 void printLL(Node* head){
     while(head != NULL){
         cout << head->data << "<->" ;
